2e2a2332bc682d2250f83040f227a44c37846cb1
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2024-01-27 21:20:11 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,1 @@
+let botaoVoltarAoTopo=document.querySelector(".voltar-ao-topo"),plataformas=document.querySelector(".btn-plataforma .plataformas"),botao=document.querySelector(".btn-plataforma");botao.addEventListener("click",()=>{plataformas.classList.toggle("ativo")});
